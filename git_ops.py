cca277d4835c19284b27546127449c3d04adc87b
Nidhish7 <nidhishraj.nr@gmail.com>
2025-06-04 09:00:26 +0530

    Modified the python script with emojies.

--- git_ops.py
+++ git_ops.py
@@ -1,19 +1,24 @@
+import sys
 import subprocess
 
+sys.stdout.reconfigure(encoding='utf-8')
+
+
 def git_status():
-    print("Checking the git status...")
+    print("\nChecking the git status...")
     try:
         subprocess.run(["git", "status"])
     except subprocess.CalledProcessError:
-        print("Failed to check Git Status.")
+        print("\n❌ Failed to check Git Status.")
         
         
 def git_add():
-    print("Adding to git...")
+    print("\nAdding to git...")
     try:
         subprocess.run(["git", "add", "."], check=True)
+        print("\n✅ Added the local files to git")
     except subprocess.CalledProcessError:
-        print("Failed to add files")
+        print("\n❌ Failed to add files")
         
         
 def has_changes_to_commit():
@@ -22,29 +27,30 @@ def has_changes_to_commit():
         print(f"Git status --porcelain output:\n{result.stdout!r}")  #debug print
         return result.stdout.strip() != ""
     except subprocess.CalledProcessError:
-        print("Failed to check the changes.")
+        print("\n❌ Failed to check the changes.")
         return False
     
     
 def git_commit(message):
-    print("Committing the changes...")
+    print("\nCommitting the changes...")
     try:
         subprocess.run(["git", "commit", "-m", message], check=True)
+        print("\n✅ Successfully committed the changes")
     except subprocess.CalledProcessError:
-        print("Failed to commit the changes.")
+        print("\n❌ Failed to commit the changes.")
     
     
 def git_push(first_push = True):
-    print("Pushing the changes the GitHub...")
+    print("\nPushing the changes the GitHub...")
     try:
         if first_push:
             subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
-            print("Successfully pushed the changes to Github")
+            print("\n✅ Successfully pushed the changes to Github")
         else:
             subprocess.run(["git", "push"], check=True)
-            print("Successfully pushed the changes to Github")
+            print("\n✅ Successfully pushed the changes to Github")
     except subprocess.CalledProcessError:
-        print("Failed to push the changes.")
+        print("\n❌ Failed to push the changes.")
 
 
 
@@ -52,13 +58,16 @@ def git_push(first_push = True):
 
 if __name__ == "__main__":
     
-    git_status()
+    try:
+        git_status()
 
-    if has_changes_to_commit():
-        git_add()
-        message = input("Enter the commit message you would like to proceed with.")
-        git_commit(message)
-        git_push()
-    else:
-        print("No Changes to commit.")
+        if has_changes_to_commit():
+            git_add()
+            message = input("\nEnter the commit message you would like to proceed with: ")
+            git_commit(message)
+            git_push()
+        else:
+            print("\n✅ No Changes to commit.")
+    except Exception as e:
+        print(f"🚨Unexpected error: {e}")
         
